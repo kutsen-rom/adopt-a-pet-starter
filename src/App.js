d3ac4e3c95f37197207772eff4d99d51df2c640b
navsi100rom <navsi100rom@users.noreply.github.com>
2022-08-12 18:40:34 +0300

    Delete yarn_lock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import HomePage from './pages/home';
-import SearchPage from './pages/search';
-import PetDetailsPage from './pages/detail';
 import Navigation from './components/navigation';
 // import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
